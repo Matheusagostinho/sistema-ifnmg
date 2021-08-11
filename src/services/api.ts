f0ce8483631ed3472728299fda7cbe309620be89
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-08-11 12:04:17 -0300

    Implentando o Admin

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/' //'https://api-ajudai.herokuapp.com/api'
+  baseURL: '/api' //'https://api-ajudai.herokuapp.com/api'
 })
